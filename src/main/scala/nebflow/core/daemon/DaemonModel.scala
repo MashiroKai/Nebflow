c70efbc6227ebfcd9a290e435c01df23edb159c0
MashiroKai <MashiroKai@users.noreply.github.com>
2026-08-05 13:43:54 +0800

    fix: DaemonConfig command accepts both String and List formats
    daemons.json written as "command": "python3 -m http.server 9999"
    failed to parse because command was List[String]. Custom decoder
    now splits string by whitespace, keeping array format as-is.

--- src/main/scala/nebflow/core/daemon/DaemonModel.scala
+++ src/main/scala/nebflow/core/daemon/DaemonModel.scala
@@ -17,7 +17,26 @@ case class DaemonConfig(
 
 object DaemonConfig:
   given Encoder[DaemonConfig] = deriveEncoder
-  given Decoder[DaemonConfig] = deriveDecoder
+  given Decoder[DaemonConfig] = Decoder.instance { c =>
+    for
+      id <- c.downField("id").as[String]
+      name <- c.downField("name").as[String]
+      commandRaw <- c.downField("command").as[Json]
+      command = commandRaw.asString match
+        case Some(s) => s.split("\\s+").toList.filter(_.nonEmpty)
+        case None => commandRaw.as[List[String]].getOrElse(Nil)
+      cwd <- c.downField("cwd").as[Option[String]]
+      env <- c.downField("env").as[Option[Map[String, String]]]
+      autoStart <- c.downField("autoStart").as[Option[Boolean]]
+      restartOnExit <- c.downField("restartOnExit").as[Option[Boolean]]
+    yield DaemonConfig(
+      id, name, command,
+      cwd,
+      env.getOrElse(Map.empty),
+      autoStart.getOrElse(false),
+      restartOnExit.getOrElse(false)
+    )
+  }
 
 /** Runtime status of a managed daemon. */
 enum DaemonStatus derives Encoder, Decoder:
